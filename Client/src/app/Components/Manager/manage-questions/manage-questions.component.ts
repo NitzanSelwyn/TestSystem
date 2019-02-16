3fb09f2febbf2f6526e59ba091ad4f8f0ddb3137
nitzan selwyn <nitzansel@gmail.com>
2019-02-16 12:31:31 +0200

    continue with showig question in a table

--- Client/src/app/Components/Manager/manage-questions/manage-questions.component.ts
+++ Client/src/app/Components/Manager/manage-questions/manage-questions.component.ts
@@ -14,9 +14,9 @@ export class ManageQuestionsComponent implements OnInit {
   id: string;
   subjectname: string;
 
-  displayedColumns = ['id', 'Qustion', 'LastUpdate', 'QustionType', 'NumberTests', 'Buttons'];
-  dataSource: MatTableDataSource<UserData>;
-  questions: UserData[] = [];
+  displayedColumns = ['QuestionId', 'Qustion', 'LastUpdate', 'QustionType', 'NumberTests', 'Buttons'];
+  dataSource: MatTableDataSource<Questions>;
+  questions: Questions[] = [];
 
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -25,10 +25,6 @@ export class ManageQuestionsComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute, private authService: AuthenticationService) { }
 
   ngOnInit() {
-    for (let i = 1; i <= 100; i++) { this.questions.push(createNewUser(i)); }
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(this.questions);
     this.showSpinner = true;
     this.route.paramMap.subscribe(params => {
       const subjectid = params.get('subjectid');
@@ -36,8 +32,11 @@ export class ManageQuestionsComponent implements OnInit {
       const organizationId = params.get('organizationId');
       this.id = subjectid;
       this.subjectname = name;
-      this.authService.GetQuestionBySubjectId(organizationId,subjectid).subscribe((data) =>{
+      this.authService.GetQuestionBySubjectId(organizationId, subjectid).subscribe((data) => {
         console.log(data);
+        this.questions = data;
+        this.dataSource = new MatTableDataSource(this.questions);
+
         this.showSpinner = false;
       });
     })
@@ -58,29 +57,24 @@ export class ManageQuestionsComponent implements OnInit {
 
 
 /** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+// function createNewUser(id: number): UserData {
+//   const name =
+//     NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+//     NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
 
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-  };
-}
+//   return {
+//     id: id.toString(),
+//     name: name,
+//     progress: Math.round(Math.random() * 100).toString(),
+//     color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+//   };
+// }
 
-/** Constants used to fill up our data base. */
-const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
-  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
-const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
-  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
-  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
 
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  color: string;
+
+export interface Questions {
+  QuestionId: string;
+  Title: string;
+  TextBelow: string;
+  IsMultyChoice: string;
 }
